3c53d4bef62e8dd9bab1aa498b2b9caef551e6db
진이지 <w2018@e-mirim.hs.kr>
2021-08-31 10:52:38 +0900

    package 2

--- moudule, package/do.py
+++ moudule, package/do.py
@@ -1,27 +1,30 @@
-import my_module as mm
-mm.정구기()
-print(mm.date)
+# import my_module as mm
+# mm.정구기()
+# print(mm.date)
+#
+# import travel.gangreung as ga
+# gp = ga.GangreungPackage()
+# print(gp)
+#
+# from travel import gangreung
+# gp = gangreung.GangreungPackage()
+# print(gp)
+#
+# from travel.gangreung import GangreungPackage
+# gp = GangreungPackage()
+# print(gp)
+#
+# from travel import ilsan
+# ilsan.이지()
+#
+# import travel.ilsan
+# travel.ilsan.이지()
+#
+# from travel.ilsan import 이지
+# 이지()
 
-import travel.gangreung as ga
-gp = ga.GangreungPackage()
-print(gp)
-
-from travel import gangreung
+from travel import *
+ilsan.이지()
 gp = gangreung.GangreungPackage()
 print(gp)
-
-from travel.gangreung import GangreungPackage
-gp = GangreungPackage()
-print(gp)
-
-from travel import ilsan
-ilsan.이지()
-
-import travel.ilsan
-travel.ilsan.이지()
-
-from travel.ilsan import 이지
-이지()
-
-
 
