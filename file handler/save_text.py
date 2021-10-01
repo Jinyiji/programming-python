8937b42bed321dfb512b10bb0526bbf49b27caef
Jinyiji <w2018@e-mirim.hs.kr>
2021-10-01 09:57:42 +0900

    save text

---
+++ file handler/save_text.py
@@ -0,0 +1,16 @@
+# try:
+#     f = open('text.txt', 'w')
+#
+#     f.write('hello')
+#     f.write('월드')
+# except:
+#     pass
+# finally:
+#     f.close()
+
+f = open('text.txt', 'w')
+
+f.write('hello')
+f.write('월드')
+
+f.close()
