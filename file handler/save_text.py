83c3d232545ccfb3c2e9c583bea9da202c527b11
Jinyiji <w2018@e-mirim.hs.kr>
2021-10-05 11:07:14 +0900

    save text

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,3 +1,4 @@
+#  정석
 # try:
 #     f = open('text.txt', 'w')
 #
@@ -15,3 +16,9 @@ f.write('\n')
 f.write('월드')
 
 f.close()
+
+print('with')
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.write('hello')
+    f.write('\n')
+    f.write('월드')
