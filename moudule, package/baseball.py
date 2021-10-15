a7b812333e9c2d9315cacec0f29138340f789e3d
Jinyiji <w2018@e-mirim.hs.kr>
2021-10-15 10:11:30 +0900

    top 3, save_history, load_history

--- moudule, package/baseball.py
+++ moudule, package/baseball.py
@@ -3,9 +3,34 @@ from custom_error import InvalidCountError
 answer = make_quiz()
 # print(answer)
 # 무한반복
+count = 0
+
+
+def save_history(player, count):
+    with open('baseball_history.txt', 'a') as f:
+        f.write(f'{player}\t{count}\n')
+
+def load_history():
+    count_list = []
+    with open('baseball_history.txt', 'r') as f:
+        while True:
+            line = f.readline()
+            if line == '' or not line:
+                break
+            # print(line.rstrip())
+            line_data = line.split('\t')
+            count_list.append(line_data[1])
+        count_list.sort()
+        return count_list[:3]
+
+print(answer)
 while True:
-#  숫자 3자리 중복없이 묻자
-    player = input("숫자 세자리는?")  # player: "123" "fun"
+    # 숫자 3자리 중복없이 묻자
+    player = input("숫자 세자리는?(t: top3)")  # player: "123" "fun"
+    if player == 't':
+        history = load_history()
+        print(history)
+        continue
     try:    # 숫자가 아닐 시 에러 처리
         player_int = int(player)        # ValueError
     except ValueError:
@@ -17,11 +42,14 @@ while True:
         continue
 
 #  strike, ball 확인하자
+    count += 1
     strike, ball = check(answer, player)
 # 출력하자
-    print(f'{player}\tstrick: {strike}\tball: {ball}')
+    print(f'{player}\tstrick: {strike}\tball: {ball}\t{count}try')
 # strike 가 3일 때, 나가자
     if strike == 3:
+        # 저장하자
+        save_history(player, count)
         break
 
 # 축하해주자
