98a8e75cc89873e52601dffe9192cae113ab9ab1
Jinyiji <w2018@e-mirim.hs.kr>
2021-10-29 10:24:21 +0900

    tictactoe game engine set winner

--- tictactoe/tictacetoe_game_engine.py
+++ tictactoe/tictacetoe_game_engine.py
@@ -13,19 +13,40 @@ class TictactoeGameEngine:
     def set(self, row, col):    # 송이김밥
         self.board[self.SIZE*(row-1)*(col-1)] = self.turn
 
+    def position_to_index(self, row, col):
+        return self.SIZE * (row - 1) + (col - 1)
+
     def set_winner(self):       # 쓰리라프
-        # - 3 줄
-        # | 3 줄
+        # 가로 3 줄
+        for row in range(1, 3 + 1):     # row: 1~3 반복
+            if self.board[self.position_to_index(row, 1)] \
+                    == self.board[self.position_to_index(row, 2)] \
+                    == self.board[self.position_to_index(row, 3)] \
+                    == self.turn:  # '.'일 때, 끝 안나게 하자.
+                return self.turn
+        # 세로 3 줄
+        for col in range(1, 3 + 1):     # col: 1~3 반복
+            if self.board[self.position_to_index(1, col)] \
+                    == self.board[self.position_to_index(2, col)] \
+                    == self.board[self.position_to_index(3, col)] \
+                    == self.turn:  # '.'일 때, 끝 안나게 하자.
+                return self.turn
         # \
+        if self.board[self.position_to_index(1, 1)] \
+                == self.board[self.position_to_index(2, 2)] \
+                == self.board[self.position_to_index(3, 3)] \
+                == self.turn:  # '.'일 때, 끝 안나게 하자.
+            return self.turn
         # /
+        if self.board[self.position_to_index(1, 3)] \
+                == self.board[self.position_to_index(2, 2)] \
+                == self.board[self.position_to_index(3, 1)] \
+                == self.turn:   # '.'일 때, 끝 안나게 하자.
+            return self.turn
         # return self.turn
         # 비기는 조건 : 디 채워졌을 때 위의 것에 해당되지 않았을 때 : self.board 에 '.'이 없는 상태  # 스우파 도윤
-        # print('X len: ', len(self.board['X']))
-        # print('O len: ', len(self.board['O']))
-        # if len(self.board['X']) + len(self.board['O']) == 9:
-        #     return 'd'      # draw
-
-
+        if not '.' in self.board:       # self.board 안에 '.'이 없다면
+            return 'd'        # draw
 
     def change_turn(self):      # 모두의 틱택토
         # self. turn 'X' 면 'O' 틀리면 'X'로 바꾸자
@@ -46,7 +67,12 @@ if __name__ == '__main__':      # main + ctrl + space bar
     print(game_engine.set_winner())     # 'X'
     game_engine.change_turn()
     print(game_engine.turn)             # 'O'
-    # 무승부 만들자, 확인하자
-    game_engine.board = ['X', 'O', 'X',
-                         'O', 'O', 'X',
-                         'X', 'X', 'O']
+    # # 무승부 만들자, 확인하자
+    # game_engine.board = ['X', 'O', 'X',
+    #                      'O', 'O', 'X',
+    #                      'X', 'X', 'O']
+    # print(game_engine.set_winner())  # 'd'
+    game_engine.set(1, 3)
+    game_engine.set(2, 2)
+    game_engine.set(3, 1)
+    print(game_engine.set_winner())     # '/'
